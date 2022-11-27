57c9e7e736463fc901c0a9e45d8c912cd77a3731
Vinicius José <viniciusjosedev@gmail.com>
2022-11-27 17:48:21 -0300

    requisito 4 finalizado

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,5 +1,9 @@
+import Title from './Title';
+
 export default function SolarSystem() {
   return (
-    <div data-testid="solar-system" />
+    <div data-testid="solar-system">
+      <Title headline="Planetas" />
+    </div>
   );
 }
